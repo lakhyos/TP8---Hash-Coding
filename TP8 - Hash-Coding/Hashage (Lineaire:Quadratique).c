04a7b57ed0c3370ddafe41f62be7109ea3273029
lakhyos <lakhyos@outlook.com>
2014-01-24 22:22:52 +0000

    Ajout des hachages Lineaire et Quadratique (Juste l’initialisation de la structure).

---
+++ TP8 - Hash-Coding/Hashage (Lineaire:Quadratique).c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  TP8 - Hash-Coding
+//
+//  Created by Med Ayoub on 21/01/2014.
+//  Copyright (c) 2014 Med Ayoub. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+
+typedef struct tab
+{
+    char *cle;
+    int element;
+}TABLE;
+
